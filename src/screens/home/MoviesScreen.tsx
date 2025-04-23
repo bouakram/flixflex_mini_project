b883f525a731e73721105f3052557cb2e0124897
bouakram <kikoua203@gmail.com>
2025-04-23 16:04:16 +0100

    migrating the movies screen, to RTK Query fetching movies and top rated moies

--- src/screens/home/MoviesScreen.tsx
+++ src/screens/home/MoviesScreen.tsx
@@ -3,7 +3,7 @@ moview screen with section of top rated movies and a section for the list of pop
 pressing on a card will take you to the detail screen of a specific movie.
 */
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -11,38 +11,60 @@ import {
   ActivityIndicator,
   StatusBar,
 } from 'react-native';
-import { getTopRatedMovies, getPopularMovies } from '../../services/apiService';
 import { COLORS } from '../../constants/styles';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import useFetch from '../../hooks/useFetch';
 import TopRateedList from '../../components/listItem/topRatedList';
 import List from '../../components/listItem/list';
+import {MoviesResponeType, useGetMoviesQuery, useGetTopRatedMoviesQuery } from '../../store/movies/moviesAPISlice';
 
 export default function MoviesScreen() {
   const insets = useSafeAreaInsets(); // to handlethe top safe area
-    // costum hook to fetch data and fetch data when loading more (pagination)
-    const {topRated, popular, loading, loadMore, loadingMore} = useFetch(getTopRatedMovies, getPopularMovies);
+  const [page, setPage] = React.useState(1);
+  const [moviesList, setMoviesList] = useState<MoviesResponeType['results']>([]);
+  const { data: mviesList, isFetching: moviesFetching } = useGetMoviesQuery(page);
+  const { data: topRatedList, isFetching: topRatedFetching } = useGetTopRatedMoviesQuery();
 
-  // handling when fetching data
-  if (loading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color={COLORS[0].primary} />
-      </View>
-    );
+  useEffect(()=>{
+  if (mviesList?.results) {
+    if (page === 1) {
+      setMoviesList(mviesList.results);
+    }else {
+      setMoviesList(prevMoviesList =>  [...prevMoviesList, ...mviesList.results] );
+    }
   }
+  },[mviesList?.results]);
+
+  const loadMore = () => {
+  if (mviesList && !moviesFetching && page <= mviesList.total_pages) {
+      setPage(page + 1);
+    }
+  };
 
   return (
-    <View style={[styles.container, {paddingTop: insets.top}]}>
-      <StatusBar backgroundColor={COLORS[0].background} barStyle={'light-content'}/>
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <StatusBar backgroundColor={COLORS[0].background} barStyle={'light-content'} />
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Top Rated Movies</Text>
-        <TopRateedList topRated={topRated} type={'movie'}/>
+        {
+          topRatedFetching ?
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color={COLORS[0].primary} />
+            </View>
+            :
+            <TopRateedList topRated={topRatedList?.results} type={'movie'} />
+        }
       </View>
 
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Popular Movies</Text>
-        <List loadMore={loadMore} loadingMore={loadingMore} itemList={popular} type={'movie'}/>
+        {
+          moviesList.length === 0 && moviesFetching ?
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color={COLORS[0].primary} />
+            </View>
+            :
+            <List loadMore={loadMore} loadingMore={moviesFetching} itemList={moviesList} type={'movie'} />
+        }
       </View>
     </View>
   );
@@ -53,9 +75,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS[0].background,
   },
-  moviesListContainer: {paddingBottom: 350},
+  moviesListContainer: { paddingBottom: 350 },
   loadingContainer: {
-    flex: 1,
+    // flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: COLORS[0].background,
